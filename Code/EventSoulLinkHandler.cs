0e3f1b9cbca95dda6f1cb6ea67bb1a990abec009
doomy64 <doomy64@gmail.com>
2026-03-20 16:08:01 -0500

    Make all events 'Shared', allowing (scuffed) voting

---
+++ Code/EventSoulLinkHandler.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Events;
+using MegaCrit.Sts2.Core.Multiplayer.Game;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Runs;
+
+namespace SoulLink.Code;
+
+[HarmonyPatch(typeof(EventSynchronizer))]
+public class EventSoulLinkHandler
+{
+    [HarmonyPatch("IsShared", MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool SynchronizerIsSharedPatch(NEventRoom __instance, ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+    
+    [HarmonyPatch("BeginEvent")]
+    [HarmonyPrefix]
+    private static void ModelIsSharedPatch(EventSynchronizer __instance, EventModel canonicalEvent, bool isPrefinished, Action<EventModel>? debugOnStart)
+    {
+        Traverse.Create(canonicalEvent).Field("<IsShared>k__BackingField").SetValue(true);
+    }
+}
