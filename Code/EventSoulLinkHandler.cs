1470bb7b780ce0d6f02454dd6e327abafea4f150
doomy64 <doomy64@gmail.com>
2026-03-20 17:08:00 -0500

    Voting for an option no longer hides options

--- Code/EventSoulLinkHandler.cs
+++ Code/EventSoulLinkHandler.cs
@@ -1,7 +1,9 @@
 ﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Events;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Events;
 using MegaCrit.Sts2.Core.Multiplayer.Game;
+using MegaCrit.Sts2.Core.Nodes.Events;
 using MegaCrit.Sts2.Core.Nodes.Rooms;
 using MegaCrit.Sts2.Core.Rooms;
 using MegaCrit.Sts2.Core.Runs;
@@ -11,6 +13,7 @@ namespace SoulLink.Code;
 [HarmonyPatch(typeof(EventSynchronizer))]
 public class EventSoulLinkHandler
 {
+    private static bool InsideButtonPress = false;
     [HarmonyPatch("IsShared", MethodType.Getter)]
     [HarmonyPrefix]
     private static bool SynchronizerIsSharedPatch(NEventRoom __instance, ref bool __result)
@@ -21,8 +24,33 @@ public class EventSoulLinkHandler
     
     [HarmonyPatch("BeginEvent")]
     [HarmonyPrefix]
-    private static void ModelIsSharedPatch(EventSynchronizer __instance, EventModel canonicalEvent, bool isPrefinished, Action<EventModel>? debugOnStart)
+    private static void BeginEventPatch(EventSynchronizer __instance, EventModel canonicalEvent, bool isPrefinished, Action<EventModel>? debugOnStart)
     {
         Traverse.Create(canonicalEvent).Field("<IsShared>k__BackingField").SetValue(true);
     }
+
+    [HarmonyPatch(typeof(NEventRoom))]
+    [HarmonyPatch("OptionButtonClicked")]
+    [HarmonyPrefix]
+    private static void OptionButtonClickedPrefix(NEventRoom __instance, EventOption option, int index)
+    {
+        InsideButtonPress = true;
+    }
+    
+    [HarmonyPatch(typeof(NEventRoom))]
+    [HarmonyPatch("OptionButtonClicked")]
+    [HarmonyPostfix]
+    private static void OptionButtonClickedPostfix(NEventRoom __instance, EventOption option, int index)
+    {
+        InsideButtonPress = false;
+    }
+
+    [HarmonyPatch(typeof(NEventLayout))]
+    [HarmonyPatch("ClearOptions")]
+    [HarmonyPrefix]
+    private static bool ClearOptionsPrefix(NEventLayout __instance)
+    {
+        return !InsideButtonPress;
+    }
+    
 }
